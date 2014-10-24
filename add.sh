9c779302bf9f0944e13276164cfb1bd62e144de5
hecj <275070023@qq.com>
2014-10-25 01:08:45 +0800

    push commit.sh

---
+++ add.sh
@@ -0,0 +1,10 @@
+git add pom.xml
+cd ../lucene-dao
+git add pom.xml
+cd ../lucene-util
+git add pom.xml
+cd ../lucene-services
+git add pom.xml
+cd ../lucene-web
+git add pom.xml
+echo 'add success!'
---
+++ commit.sh
@@ -0,0 +1,10 @@
+git commit -m "push pom.xml"
+cd ../lucene-dao
+git commit -m "push pom.xml"
+cd ../lucene-util
+git commit -m "push pom.xml"
+cd ../lucene-services
+git commit -m "push pom.xml"
+cd ../lucene-web
+git commit -m "push pom.xml"
+echo 'add success!'
